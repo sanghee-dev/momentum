cb6cf113ce2826402de2c25b8749910f7512f333
leeedev <leeesangheee@gmail.com>
2020-10-11 23:34:53 +0900

    Complete greeting

---
+++ src/greeting.js
@@ -0,0 +1,48 @@
+const form = document.querySelector(".js-form"),
+  input = document.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
+
+// currentUser O
+function paintGreeting(currentUser) {
+  form.classList.remove("showing");
+  greeting.classList.add("showing");
+
+  const date = new Date();
+  const hours = date.getHours();
+
+  if (6 <= hours >= 6 && hours < 12) {
+    greeting.innerHTML = `Good morning, ${currentUser}.`;
+  } else if (12 <= hours && hours < 18) {
+    greeting.innerHTML = `Good afternoon, ${currentUser}.`;
+  } else {
+    greeting.innerHTML = `Good evening, ${currentUser}.`;
+  }
+}
+
+// X -- > submit --> set
+function handlesubmit(event) {
+  event.preventDefault();
+  localStorage.setItem("currentUser", input.value);
+  paintGreeting(input.value);
+}
+
+// currentUser X
+function askForName() {
+  form.classList.add("showing");
+  form.addEventListener("submit", handlesubmit);
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem("currentUser");
+  if (currentUser) {
+    paintGreeting(currentUser);
+  } else {
+    askForName();
+  }
+}
+
+function init() {
+  loadName();
+}
+
+init();
