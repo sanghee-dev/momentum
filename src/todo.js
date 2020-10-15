732c2b9a92a182e09294c7b74b9914cbab44081d
leeedev <leeesangheee@gmail.com>
2020-10-15 18:45:39 +0900

    Add paintToDot() in todo.js

--- src/todo.js
+++ src/todo.js
@@ -2,8 +2,21 @@ const toDoForm = document.querySelector(".js-toDo-form"),
   toDoInput = document.querySelector(".js-toDo-input"),
   toDoList = document.querySelector(".js-toDo-list");
 
+function paintToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.innerHTML = "❌";
+  const span = document.createElement("span");
+  span.innerHTML = text;
+
+  li.appendChild(delBtn);
+  li.appendChild(span);
+  toDoList.appendChild(li);
+}
+
 function handleSubmit(event) {
   event.preventDefault();
+  paintToDo(toDoInput.value);
   toDoInput.value = "";
 }
 
