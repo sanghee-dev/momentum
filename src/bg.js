c766a42e4b84e83b810156f78894b4f71276670a
leeedev <leeesangheee@gmail.com>
2020-10-19 16:54:27 +0900

    Complete bg.js

---
+++ src/bg.js
@@ -0,0 +1,23 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 5;
+
+function paintImage(imgnumber) {
+  console.log(imgnumber);
+  const image = new Image();
+  image.src = `images/${imgnumber + 1}.jpg`;
+  image.classList.add("bgImage");
+  body.appendChild(image);
+}
+
+function genRandom() {
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
+}
+
+function init() {
+  const randomNumber = genRandom();
+  paintImage(randomNumber);
+}
+
+init();
